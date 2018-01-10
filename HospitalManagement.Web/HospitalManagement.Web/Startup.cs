31f650136405833be249d7fce708e326f3046bd3
Kamol Roy <kamol.roy9943@gmail.com>
2018-01-10 20:26:34 +0600

    Initial commit

---
+++ HospitalManagement.Web/HospitalManagement.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HospitalManagement.Web.Startup))]
+namespace HospitalManagement.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
