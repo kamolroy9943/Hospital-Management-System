76a26462f8f638c418fd0a0afa14b61ef32125bc
Mahfuzur-Rahman <7mahfuzur.rahman@gmail.com>
2018-01-13 23:47:10 +0600

    Re-resolved the untracked contxt file

---
+++ HospitalManagement.Web/HospitalManagement.Web/Models/HospitalManagementContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using HospitalManagement.Data;
+
+namespace HospitalManagement.Web.Models
+{
+    public class HospitalManagementContext : DbContext
+    {
+
+        public DbSet<Building> Building { get; set; }
+    }
+}
