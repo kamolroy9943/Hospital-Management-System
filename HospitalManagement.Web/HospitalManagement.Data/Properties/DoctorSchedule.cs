34bac90c033a17acd2efaf4422f53c55021f8a14
Mahfuzur-Rahman <7mahfuzur.rahman@gmail.com>
2018-02-03 10:34:45 +0600

    Doctors Schedule Curd Operation Added

---
+++ HospitalManagement.Web/HospitalManagement.Data/Properties/DoctorSchedule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Data
+{
+    public class DoctorSchedule
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int DoctorId { get; set; }
+        public string DoctorName { get; set; }
+        public Doctor Doctor { get; set; }
+
+        public string Schedule { get; set; }
+
+        public DateTime Updated { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
